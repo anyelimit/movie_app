b63ce29141f4f6e3c514135fd47f1206e26008a4
hyelimimi <hyelim6n@gmail.com>
2020-03-01 15:49:06 +0900

    Updates

---
+++ precache-manifest.ff180b5b0954aee87d2409a547e49ade.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec9055aa103c1bad81519a015d95471",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "ff31a43c949c6c7e2c58",
+    "url": "/movie_app/static/css/main.6cb46e83.chunk.css"
+  },
+  {
+    "revision": "70e84318d65e22caa855",
+    "url": "/movie_app/static/js/2.1c932c0d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app/static/js/2.1c932c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff31a43c949c6c7e2c58",
+    "url": "/movie_app/static/js/main.7aed83a0.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
